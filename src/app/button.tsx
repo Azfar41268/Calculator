3d5e42a3f177b06d0ab9beb0a2fef9086d7bb783
Azfar41268 <azfarsikander33@gmail.com>
2024-04-02 20:02:19 +0500

    gg

--- src/app/button.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function Button({ children }: any){
-    return(
-        <button className="lg:w-16 lg:h-16 md:h-14 md:w-14 h-12 w-12 text-lg font-medium rounded-full hover:scale-105 active:scale-95 transition-all border-black border-2 ">
-            {children}
-        </button>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 'use client'
 
+import Functions from "@/components/Functions";
+import Top from "@/components/Top";
+import NumberPad from "@/components/numberPad";
 import { useState } from "react";
-import Button from "./button";
+
 
 
 export default function Home() {
   const [current, setCurrent] = useState('')
+  let name = "lg:w-16 lg:h-16 md:h-14 md:w-14 h-12 w-12 text-lg font-medium rounded-full hover:scale-105 active:scale-95 transition-all border-black border-2"
+
+  function factorial(n: number): number {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
 
   function evaluate(equation: string) {
     for (let i = 0; i < equation.length; i++) {
@@ -15,6 +27,13 @@ export default function Home() {
         equation = equation.replace('÷', '/')
       } else if (equation[i] === '^') {
         equation = equation.replace('^', '**')
+      } else if (equation[i] === '✓') {
+        equation = equation.replace('✓', '**(1/2)')
+      } else if (equation[i] === '!') {
+        let k: number = Number(equation[i - 1])
+        let total: number = factorial(k)
+        equation = equation.replace(equation[i-1], `${total}`)
+        equation = equation.replace(equation[i], '')
       }
     }
     
@@ -27,84 +46,21 @@ export default function Home() {
       }, 3000)
     } catch (error) {
       setCurrent("Error in expression");
+      setTimeout(() => {
+        setCurrent(equation)
+      }, 3000)
     }
   }
 
   return (
     <div className="flex justify-center items-center h-screen w-12/12 bg-gradient-to-tr from-black via-indigo-800 to-blue-900 py-10">
       <div className="flex flex-col items-center justify-between p-4 space-y-4 rounded-lg bg-slate-400/15 w-screen lg:w-6/12">
-        <h1 className="text-2xl sm:text-3xl lg:text-4xl font-semibold text-white">
-          Calculator
-        </h1>
-        <input type="text" onKeyDown={(event) => {
-          if (event.key === 'Enter') {
-            evaluate(current)
-          }
-        }} value={current} onChange={(e) => setCurrent(e.currentTarget.value)} className="h-12 py-3 w-full px-4 rounded-md border-black/40 border-4 bg-transparent text-white" />
+        <Top setCurrent={setCurrent} current={current} evaluate={evaluate} />
         <div className="flex flex-col md:flex-row justify-center items-center px-4 py-4 text-white space-y-3">
           {/* Numbers */}
-          <div className="flex flex-wrap space-x-2 space-y-2 w-full lg:w-64 md:w-56 justify-center items-center">
-            <Button onClick={() => setCurrent(current + '1')}>
-              1
-            </Button>
-            <Button onClick={() => setCurrent(current + '2')}>
-              2
-            </Button>
-            <Button onClick={() => setCurrent(current + '3')}>
-              3
-            </Button>
-            <Button onClick={() => setCurrent(current + '4')}>
-              4
-            </Button>
-            <Button onClick={() => setCurrent(current + '5')}>
-              5
-            </Button>
-            <Button onClick={() => setCurrent(current + '6')}>
-              6
-            </Button>
-            <Button onClick={() => setCurrent(current + '7')}>
-              7
-            </Button>
-            <Button onClick={() => setCurrent(current + '8')}>
-              8
-            </Button>
-            <Button onClick={() => setCurrent(current + '9')}>
-              9
-            </Button>
-            <Button onClick={() => setCurrent(current + '0')}>
-              0
-            </Button>
-            <Button onClick={() => setCurrent(current + '.')}>
-              .
-            </Button>
-          </div>
+          <NumberPad setCurrent={setCurrent} current={current} />
           {/* Functions */}
-          <div className="flex flex-wrap space-x-2 space-y-2 w-full lg:w-64 md:w-56 justify-center items-center">
-            <Button onClick={() => setCurrent(current + '+')}>
-                +
-              </Button>
-            <Button onClick={() => setCurrent(current + '-')}>
-                -
-              </Button>
-            <Button onClick={() => setCurrent(current + 'x')}>
-                x
-              </Button>
-            <Button onClick={() => setCurrent(current + '÷')}>
-              ÷
-            </Button>
-            <Button onClick={() => setCurrent(current + '^')}>
-              x^
-            </Button>
-            <Button onClick={() => evaluate(current)}>
-                =
-            </Button>
-            <Button onClick={() => setCurrent(current.slice(0, current.length -1))}>
-                {'<-'}
-            </Button>
-            <Button onClick={() => setCurrent('')} >
-                Clear
-            </Button>
-          </div>
+          <Functions setCurrent= {setCurrent} current={current} evaluate={evaluate} />
         </div>
       </div>
     </div>
